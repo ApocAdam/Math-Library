58f2ff1b43337ffa535460b99f2c51e93a966072
Adam <achappell2001@gmail.com>
2021-05-05 16:32:05 +1200

    Book Schema changes and async package added

--- models/book.js
+++ models/book.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
     title: {type: String, required: true},
-    author: {type: Schema.Types.ObjectId, required: true, ref: "Author"},
-    summary: {type: String, required: true},
+    author: [{type: Schema.Types.ObjectId, required: true, ref: "Author"}],
+    summary: {type: String},
     field: {type: Schema.Types.ObjectId, required: true, ref: "Field"},
     difficulty: {type: Schema.Types.ObjectId, required: true, ref: "Difficulty"},
-    publisher: {type: Schema.Types.ObjectId, required: true, ref: "Publisher"}
+    publisher: {type: Schema.Types.ObjectId, ref: "Publisher"}
 });
 
 
